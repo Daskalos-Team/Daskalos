bd4d6ec3f098d3ed77c5e728454056da0c38428d
lkalandadze <lkalandadze@devexperts.com>
2023-05-07 23:18:04 +0400

    temp path fix

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthorizationPage } from "./components/authorization-page";
 import { MainPage } from "./components/main-page";
+import {ProfilePage} from "./components/profile-page";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<AuthorizationPage />}>
+                <Route path="/" element={<ProfilePage />}>
                     <Route index element={<MainPage />} />
                     <Route path="main" element={<MainPage />} />
                     <Route path="register" element={<AuthorizationPage />} />
