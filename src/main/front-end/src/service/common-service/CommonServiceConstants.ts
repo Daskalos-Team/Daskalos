badf1b9b5f97b691557547ef260bb6f1f12ad5f7
adikashvili <adikashviligiorgi@gmail.com>
2023-05-07 15:36:38 +0400

    correct endpoint

--- src/main/front-end/src/service/common-service/CommonServiceConstants.ts
+++ src/main/front-end/src/service/common-service/CommonServiceConstants.ts
@@ -1,4 +1,4 @@
-export const USER_ENDPOINT = "http://localhost:8080/user/"; // "https://daskalos.herokuapp.com/user/"
+export const USER_ENDPOINT = "https://daskalos.herokuapp.com/user/";
 export const SERVICE_ID = "service_m92suca";
 export const TEMPLATE_ID = "template_wcrd1ei";
 export const PUBLIC_KEY = "CbmNbHyLjq1ERQzPU";
