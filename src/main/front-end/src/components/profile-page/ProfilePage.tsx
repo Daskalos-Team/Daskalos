b696a9f4d04400fbbc97359e3cb8dad6a52970ba
lkalandadze <lkalandadze@devexperts.com>
2023-05-07 20:41:43 +0400

    added new page components

--- src/main/front-end/src/components/profile-page/ProfilePage.tsx
+++ src/main/front-end/src/components/profile-page/ProfilePage.tsx
@@ -19,13 +19,88 @@ export const ProfilePage = (props: any) => {
                     <div className="profile-container-inner">
                         <img src="profile-pic.png" width="100%" className={"profile-pic"} alt={"profile pic"}/>
                         <h1>Ryan Walton</h1>
+                        <div className="average-rating-subject">
+                            <span>Average Rating 5.0/5.0</span>
+                            <p>Subjects: Maths, Biology</p>
+                        </div>
                         <b>Web Developer at Microsoft | Former Developer at Amazon</b>
                         <p>San Francisco, United States</p>
-                        <div className="mutual-connection">
-                            <img src="profile-pic.png" width="100%" alt={"mutual connection pic"}/>
-                            <span>1 mutual connection: Orlando blum</span>
+                    </div>
+                </div>
+                <div className="profile-description">
+                    <h2>About</h2>
+                    <p>I am a young teacher, with passion for teaching school kids several different subjects, including Maths, Physics, Chemistry, Biology, Geography, History and Literature</p>
+                    <p>Telephone Number: +995 599 999 999</p>
+                    <p>Email: daskalosdaskalosovich@gmail.com</p>
+                </div>
+
+                {/*experience section*/}
+                <div className="profile-description">
+                    <h2>Experience</h2>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Lead Front End Developer</h3>
+                            <b>Microsoft &middot; Full-time</b>
+                            <b>Feb 2021 - Present &middot; 1.5 years</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
                         </div>
                     </div>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Lead Front End Developer</h3>
+                            <b>Microsoft &middot; Full-time</b>
+                            <b>Feb 2021 - Present &middot; 1.5 years</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
+                        </div>
+                    </div>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Lead Front End Developer</h3>
+                            <b>Microsoft &middot; Full-time</b>
+                            <b>Feb 2021 - Present &middot; 1.5 years</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/*comment section*/}
+                <div className="profile-description">
+                    <h2>Comments</h2>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Luka Kalandadze</h3>
+                            <b>Rating 5.0/5.0</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
+                        </div>
+                    </div>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Luka Kalandadze</h3>
+                            <b>Rating 5.0/5.0</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
+                        </div>
+                    </div>
+
+                    <div className="profile-desc-row">
+                        <div>
+                            <h3>Luka Kalandadze</h3>
+                            <b>Rating 5.0/5.0</b>
+                            <p>Computer programming is the process of performing a particular computation, usually by designing and building an executable computer Program.</p>
+                            <hr/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
