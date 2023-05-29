edc9e74232975464f36e1dffb14f8d8d03f8dc77
adikashvili <adikashviligiorgi@gmail.com>
2023-05-30 00:26:39 +0400

    removed extra comments

--- src/main/front-end/src/components/profile-page/ProfilePage.tsx
+++ src/main/front-end/src/components/profile-page/ProfilePage.tsx
@@ -49,18 +49,18 @@ export const ProfilePage = (): React.JSX.Element => {
             <div className="page-content">
                 <NavBar active="profile" />
 
-                {/* <svg className="cover-svg" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg"> */}
-                {/*     <defs> */}
-                {/*         <path id='sineWave' fill="#0099ff" fillOpacity="0.1" */}
-                {/*             d="M0,160 C320,300,420,300,740,160 C1060,20,1120,20,1440,160 V0 H0" /> */}
-                {/*     </defs> */}
-                {/*     <use className="wave" href="#sineWave" /> */}
-                {/*     <use className="wave" x="-100%" href="#sineWave" /> */}
-                {/*     <use className="wave1" href="#sineWave" /> */}
-                {/*     <use className="wave1" x="-100%" href="#sineWave" /> */}
-                {/*     <use className="wave2" href="#sineWave" /> */}
-                {/*     <use className="wave2" x="-100%" href="#sineWave" /> */}
-                {/* </svg> */}
+                <svg className="cover-svg" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <path id='sineWave' fill="#0099ff" fillOpacity="0.1"
+                            d="M0,160 C320,300,420,300,740,160 C1060,20,1120,20,1440,160 V0 H0" />
+                    </defs>
+                    <use className="wave" href="#sineWave" />
+                    <use className="wave" x="-100%" href="#sineWave" />
+                    <use className="wave1" href="#sineWave" />
+                    <use className="wave1" x="-100%" href="#sineWave" />
+                    <use className="wave2" href="#sineWave" />
+                    <use className="wave2" x="-100%" href="#sineWave" />
+                </svg>
 
                 <div className="content-wrapper">
                     <div className="ocean">
