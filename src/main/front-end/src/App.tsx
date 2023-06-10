fc78491b2db7e3f87d7f774da1f4d97c3b3dd640
adikashvili <adikashviligiorgi@gmail.com>
2023-06-10 17:39:32 +0400

    bug fixed

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NewsFeedPage } from "./components/news-feed-page";
 import { AuthorizationPage } from "./components/authorization-page";
-import { MainPage } from "./components/main-page";
 import { ProfilePage } from "./components/profile-page";
 import "./App.css";
 
@@ -11,8 +10,6 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<ProfilePage />}>
-                    <Route index element={<MainPage />} />
-                    <Route path="main" element={<MainPage />} />
                     <Route path="register" element={<AuthorizationPage />} />
                 </Route>
                 <Route path="/news-feed" element={<NewsFeedPage />} />
