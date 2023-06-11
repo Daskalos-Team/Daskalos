591143bcea97b2cbe8c3f2682269b91f76966fc6
lkalandadze <lkalandadze@devexperts.com>
2023-06-11 18:29:10 +0400

    started subject schedule functionality

---
+++ src/main/java/com/freeuni/daskalos/repository/SubjectScheduleRepository.java
@@ -0,0 +1,13 @@
+package com.freeuni.daskalos.repository;
+
+import com.freeuni.daskalos.repository.entities.SubjectSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubjectScheduleRepository extends JpaRepository<SubjectSchedule, Long> {
+
+    List<SubjectSchedule> findSubjectScheduleBySubjectID(Long subjectID);
+}
