1a5b1ad1ebfd2f3cc49b10f2b4f0ab8f31692472
adikashvili <adikashviligiorgi@gmail.com>
2023-06-20 13:16:20 +0400

    small change in App.tsx

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NewsFeedPage } from "./components/news-feed-page";
 import { AuthorizationPage } from "./components/authorization-page";
 import { ProfilePage } from "./components/profile-page";
 import "./App.css";
+import { MainPage } from "./components/main-page";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<ProfilePage />}>
-                    <Route path="register" element={<AuthorizationPage />} />
+                <Route path="/" element={
+                    <MainPage>
+                        <AuthorizationPage />
+                        <NewsFeedPage />
+                    </MainPage>
+                }>
+                    <Route path="*" element={<Navigate to="/" replace={true} />} />
                 </Route>
                 <Route path="/news-feed" element={<NewsFeedPage />} />
                 <Route path="/profile-page" element={<ProfilePage />} />
