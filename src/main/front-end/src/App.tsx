6a866f455c85e9c2fca509ed41b68e56712208da
adikashvili <adikashviligiorgi@gmail.com>
2023-06-20 17:20:26 +0400

    added delay animation

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MainPage } from "./components/main-page";
 import "./App.css";
 
 function App() {
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MainPage/>}>
-                    <Route path="*" element={<Navigate to="/" replace={true} />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            {loading ? (
+                <div className="loader-container">
+                    <div className="spinner-label-background"/>
+                    <div className="content">
+                        <h2>დასკალოსი</h2>
+                        <h2>დასკალოსი</h2>
+                    </div>
+                    <div className="spinner"/>
+                </div>
+            ) : (
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}>
+                            <Route path="*" element={<Navigate to="/" replace={true} />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            )}
+        </>
     );
 }
 
